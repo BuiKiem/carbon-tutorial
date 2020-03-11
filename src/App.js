8cf9afd4ba2fd456110f50ef39d5ab43dfe171ee
Bui Kiem <buivantrung1105@gmail.com>
2020-03-11 18:45:10 +0700

    feat: create 'TutorialHeader' component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Button } from 'carbon-components-react';
+import { Content } from 'carbon-components-react/es/components/UIShell';
+import TutorialHeader from './components/TutorialHeader';
 
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return <Button>Button</Button>;
-  }
-}
+const App = () => {
+  return (
+    <>
+      <TutorialHeader />
+      <Content>
+        <Button>Button</Button>
+      </Content>
+    </>
+  );
+};
 
 export default App;
